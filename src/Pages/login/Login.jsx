07fbefc2bf9e38622d38c03fd6d751754675727a
Giva Gomes <givagomes04@gmail.com>
2023-05-22 23:57:23 -0300

    alterando solicitação de cadastro

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import Home from './Home/Home';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Home from "../Home/Home";
+import { Link } from "react-router-dom";
 
 function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -17,16 +17,16 @@ function LoginForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Aqui você pode adicionar a lógica para autenticar o usuário com o nome de usuário e senha fornecidos
-    console.log('Nome de usuário:', username);
-    console.log('Senha:', password);
+    console.log("Nome de usuário:", username);
+    console.log("Senha:", password);
     // Você também pode enviar os dados para um servidor para realizar a autenticação
   };
 
   return (
-    
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="username">Nome de usuário:</label><br />
+        <label htmlFor="username">Nome de usuário:</label>
+        <br />
         <input
           type="text"
           id="username"
@@ -34,20 +34,24 @@ function LoginForm() {
           onChange={handleUsernameChange}
         />
       </div>
-      <div><br />
-        <label htmlFor="password">Senha:</label><br />
+      <div>
+        <br />
+        <label htmlFor="password">Senha:</label>
+        <br />
         <input
           type="password"
           id="password"
           value={password}
           onChange={handlePasswordChange}
         />
-      </div><br />
-      <Link to={'/home'}>
-      <button type="submit">Cadastrar</button>
-      </Link><br/>
-      <div className='cadastro'>
-        <Link to={'/cadastro'}> Click aqui</Link> para Criar uma conta
+      </div>
+      <br />
+      <Link to={"/home"}>
+        <button type="submit">Cadastrar</button>
+      </Link>
+      <br />
+      <div className="cadastro">
+        Ainda não tem uma conta? <Link to={"/cadastro"}> Click aqui</Link> 
       </div>
     </form>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.sass'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-import Login from './Pages/login.jsx';
-import Home from './Pages/Home/Home.jsx';
-import Cadastro from './Pages/Cadastro/Cadastro.jsx';
-import Carteira from './Pages/Carteira/carteira.jsx';
-import Transacao from './Pages/Transacao/transacao.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.sass";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Pages/login/Login.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import Cadastro from "./Pages/Cadastro/Cadastro.jsx";
+import Carteira from "./Pages/Carteira/carteira.jsx";
+import Transacao from "./Pages/Transacao/transacao.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
   },
-  
+
   {
     path: "/cadastro",
     element: <Cadastro />,
-  }, 
+  },
 
   {
     path: "/home",
@@ -32,15 +32,12 @@ const router = createBrowserRouter([
 
   {
     path: "/transacao",
-    element: <Transacao/>,
+    element: <Transacao />,
   },
-  
 ]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
